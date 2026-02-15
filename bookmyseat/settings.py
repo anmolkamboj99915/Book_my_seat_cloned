9d59dde3903f991a31cbc58698dacee67c581e9d
anmolkamboj99915 <anmolkamboj99915@outlook.com>
2026-02-15 16:17:01 +0530

    Full project sync1

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -21,8 +21,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost,.vercel.app"
+    "*"
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = [
